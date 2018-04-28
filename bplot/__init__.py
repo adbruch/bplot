5d84ae42406ce34dea32d539e1024a95cc26b2d1
Edward A. Roualdes <eroualdes@csuchico.edu>
2018-04-28 10:40:13 -0700

    rebuild __init__ for whatever reason

--- bplot/__init__.py
+++ bplot/__init__.py
@@ -1,4 +1,13 @@
-from .bplot import *
-
+from .box import box, box_h
+from .colors import colors
+from .density import density
+from .histogram import histogram
+from .jitter import jitter
+from .line import line
+from .point import point
+from .rug import rug
+from .std import std, std_h
+from .ste import ste, ste_h
+from .trace import trace
 
 __version__ = '0.1'
