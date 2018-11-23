58b94fb2c4d7756be202ab4624644df22e7c2c97
Edward A. Roualdes <eroualdes@csuchico.edu>
2018-11-22 21:32:16 -0800

    try new deploy script; clean makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,2 @@
 yapf:
 	yapf -r -i bplot/
-
-doc:
-	$(MAKE) -C doc html
-
-clean-doc:
-	$(MAKE) -C clean
--- _deploy.sh
+++ _deploy.sh
@@ -5,9 +5,9 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git doc-output
+git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git doc-output
 cd doc-output
-cp -r ../doc/build/html/* ./
+cp -r doc/build/html/* ./
 git add --all *
 git commit -m"Update the docs" || true
 git push -q origin gh-pages
