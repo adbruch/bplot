930f7b689df3dd91209aad94025305f8cf725906
Edward A. Roualdes <eroualdes@csuchico.edu>
2018-11-22 21:38:40 -0800

    add git config info to _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -5,9 +5,12 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git doc-output
+git config --global user.email "eroualdes@csuchico.edu"
+git config --global user.name "Edward A. Roualdes"
+
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git doc-output
 cd doc-output
-cp -r doc/build/html/* ./
+cp -r ../doc/build/html/* ./
 git add --all *
 git commit -m"Update the docs" || true
 git push -q origin gh-pages
